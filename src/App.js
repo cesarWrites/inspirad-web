f7fa7bbe437e41c962a8209c56760e9bab49626d
cesarWrites <cesarwrites24@gmail.com>
2024-07-25 00:34:26 +0300

    Modify routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import BookList from './components/BookList';
+import BookDetail from './pages/BookDetail';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/books" element={<BookList/>} />
+        <Route path="/books/:id" element={<BookDetail/>} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
+
+
